c97b97dd6fe39f76694afbf00fe3e192c85d4ad0
hnguzx <2544697696@qq.com>
2020-08-05 19:43:27 +0800

    elementui基础搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import zhLocale from "element-ui/lib/locale/lang/zh-CN";
+// import enLocale from "element-ui/lib/locale/lang/en";
+// fade 淡入淡出/zoom 缩放
+import 'element-ui/lib/theme-chalk/base.css'
+// collapse 折叠展开
+import CollapseTransition from 'element-ui/lib/transitions/collapse-transition'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, {zhLocale})
+Vue.component(CollapseTransition.name, CollapseTransition)
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
